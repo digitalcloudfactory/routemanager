3175a3c1eca30dccba4cf6fe84cb790200a6f0cc
digitalcloudfactory <37212874+digitalcloudfactory@users.noreply.github.com>
2026-01-13 18:51:12 +0100

    Create callback.php

---
+++ callback.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+
+$client_id = 'YOUR_STRAVA_CLIENT_ID';
+$client_secret = 'YOUR_STRAVA_CLIENT_SECRET';
+$redirect_uri = 'http://localhost:8080/callback.php';
+
+if (!isset($_GET['code'])) {
+    echo "No code returned from Strava.";
+    exit;
+}
+
+$code = $_GET['code'];
+
+// Exchange code for access token
+$token_url = "https://www.strava.com/oauth/token";
+$data = [
+    'client_id' => $client_id,
+    'client_secret' => $client_secret,
+    'code' => $code,
+    'grant_type' => 'authorization_code'
+];
+
+$ch = curl_init($token_url);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+curl_close($ch);
+
+$json = json_decode($response, true);
+
+if (isset($json['access_token'])) {
+    $_SESSION['access_token'] = $json['access_token'];
+    header("Location: activities.php");
+    exit;
+} else {
+    echo "Error fetching access token: " . $response;
+}
+?>
