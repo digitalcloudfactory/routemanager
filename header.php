2b91518e94712d0d73b136b5f9528d1d3887572e
digitalcloudfactory <37212874+digitalcloudfactory@users.noreply.github.com>
2026-01-13 21:49:43 +0100

    Create header.php

---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" data-theme="light">
+<head>
+  <meta charset="UTF-8">
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
+  />
+</head>
+<body>
