18b0666b8d4f48350512aedf375173fa357207b6
digitalcloudfactory <37212874+digitalcloudfactory@users.noreply.github.com>
2026-01-14 18:36:47 +0100

    Update callback.php

--- callback.php
+++ callback.php
@@ -1,5 +1,8 @@
 <?php
-// callback.php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
 header('Content-Type: text/html; charset=UTF-8');
 
