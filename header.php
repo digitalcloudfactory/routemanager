4b3684a380bdc4bf4fc77b6fa77f6650da45f27e
digitalcloudfactory <37212874+digitalcloudfactory@users.noreply.github.com>
2026-01-14 12:02:27 +0100

    Update header.php

--- header.php
+++ header.php
@@ -2,9 +2,10 @@
 <html lang="en" data-theme="light">
 <head>
   <meta charset="UTF-8">
-  <link
-    rel="stylesheet"
-    href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
-  />
+  <link rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
+<link rel="stylesheet"
+      href="https://unpkg.com/leaflet/dist/leaflet.css">
+
 </head>
 <body>
