2aac5f6efb5a6a4571a1a16434be7d77b42e542b
digitalcloudfactory <37212874+digitalcloudfactory@users.noreply.github.com>
2026-01-19 13:30:35 +0100

    Update routes.php

--- routes.php
+++ routes.php
@@ -458,7 +458,7 @@ async function saveTags(routeId, value) {
     
 </script>
 
-<script src="routes_shared.js"></script>
+<script src="routes_shared.js?v=1.0.1"></script>
 
   <script>
   const mapLink = document.getElementById('mapLink');
