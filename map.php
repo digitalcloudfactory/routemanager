8f9064ae8b176efcf29fb7f58575a9d1c1a33c79
digitalcloudfactory <37212874+digitalcloudfactory@users.noreply.github.com>
2026-01-19 13:19:49 +0100

    Update map.php

--- map.php
+++ map.php
@@ -117,7 +117,7 @@ unset($route);
          style="border-radius:50%">
     <div>
       <strong><?= htmlspecialchars($user['firstname'].' '.$user['lastname']) ?></strong><br>
-      <small>Last Strava Sync: <?= htmlspecialchars($user['last_routes_sync']) ?></small>
+      <small>Last Strava Sync:<?= $user['last_routes_sync']? htmlspecialchars($user['last_routes_sync']): '<em>Never synced</em>' ?></small>
     </div>
   </div>
 
