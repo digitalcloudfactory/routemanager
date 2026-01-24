4aa7710fe8198b0738c53bda53d667c1025b479a
digitalcloudfactory <37212874+digitalcloudfactory@users.noreply.github.com>
2026-01-24 19:04:16 +0100

    Update fetch_routes.php

--- fetch_routes.php
+++ fetch_routes.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 if (!isset($_SESSION['internal_user_id'])) {
     echo json_encode(['success' => false, 'error' => 'Not logged in']);
