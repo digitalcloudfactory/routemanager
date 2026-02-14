ecf8c1a76765a9650ed5043424a03343d88de7cb
digitalcloudfactory <37212874+digitalcloudfactory@users.noreply.github.com>
2026-02-14 15:08:12 +0100

    Update routes.php

--- routes.php
+++ routes.php
@@ -340,7 +340,7 @@ function renderTable(data) {
       <td>${Number(route.distance_km).toFixed(2)}</td>
       <td>${route.elevation}</td>
       <td>${formatDuration(route.estimated_moving_time)}</td>
-      <td>${route.starred}</td>
+      <td>${route.starred == 1 ? '&#x1F512;' : ''}</td>
       <td>${route.private}</td>
     `;
 
