d8804872af305e3fc750afd6b6c8ee47ef5a190d
digitalcloudfactory <37212874+digitalcloudfactory@users.noreply.github.com>
2026-06-19 12:48:37 +0200

    Update header.php

--- header.php
+++ header.php
@@ -2,10 +2,11 @@
 <html lang="en" data-theme="light">
 <head>
   <meta charset="UTF-8">
-  <link rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
-<link rel="stylesheet"
-      href="https://unpkg.com/leaflet/dist/leaflet.css">
-
+  <title>Strava Routes Map</title>
+  
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
+  
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css">
+  <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
 </head>
 <body>
