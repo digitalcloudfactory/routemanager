feee8a9fb5b7d5bfea87d33f9e1d6d974ae9ba9a
digitalcloudfactory <37212874+digitalcloudfactory@users.noreply.github.com>
2026-06-21 15:02:04 +0200

    Update map.php

--- map.php
+++ map.php
@@ -57,7 +57,7 @@ $stmt = $pdo->prepare("
     FROM strava_routes
     WHERE user_id = ?
     ORDER BY updated_at DESC
-    LIMIT 466
+    LIMIT 400
 ");
 $stmt->execute([$internalUserId]);
 $routes = $stmt->fetchAll(PDO::FETCH_ASSOC);
