01e0975a42ffbc9f2f7a2228da0d50d41af6028b
digitalcloudfactory <37212874+digitalcloudfactory@users.noreply.github.com>
2026-06-21 14:08:19 +0200

    Update map.php

--- map.php
+++ map.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 
+ini_set('memory_limit', '512M'); // Raise it to 512MB or higher
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
