f03fc59c0b2527145304ced74e0d153d03b1e6df
digitalcloudfactory <37212874+digitalcloudfactory@users.noreply.github.com>
2026-07-13 20:48:30 +0200

    bootstrap

--- routes.php
+++ routes.php
@@ -89,6 +89,7 @@ $countries = $countryStmt->fetchAll(PDO::FETCH_COLUMN);
 
 <!-- Bootstrap 5 CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
 <!-- Bootstrap 5 JS Bundle (Put this right before </head> or near footer.php) -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
